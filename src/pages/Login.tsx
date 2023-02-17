0a3ef4b6e87f843a79332606448a9653c243b397
elizabeth <ellizzabbetth@gmail.com>
2023-02-16 22:04:29 -0500

    login & Register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import '../Login.css'
 import styled, { css } from 'styled-components'
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 
 
-const StyledFormSignIn = styled.form`
-width: 100%;
-max-width: 330px;
-padding: 15px;
-margin: auto;
+const StyledMain = styled.main`
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
 `;
 
 const StyledCheckbox = styled.input`
@@ -28,58 +31,72 @@ const StyledFormControl = styled.input`
 `;
 
 
-const InputEmail = styled.input.attrs({ type: 'email' })`
+const InputEmail = styled(StyledFormControl).attrs({ type: 'email' })`
   margin-bottom: -1px;
   border-bottom-right-radius: 0;
   border-bottom-left-radius: 0;
 
-
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
 
   &:focus {
       z-index: 2;
   }
 }
 `;
-const InputPassword = styled.input.attrs({ type: 'password' })`
+
+
+const InputPassword = styled(StyledFormControl).attrs({ type: 'password' })`
 margin-bottom: 10px;
 border-top-left-radius: 0;
 border-top-right-radius: 0;
 
-  
-position: relative;
-box-sizing: border-box;
-height: auto;
-padding: 10px;
-font-size: 16px;
 
 &:focus {
     z-index: 2;
 }
 }
 `;
+
+
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
+    let navigate = useNavigate();
+
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        await axios.post('login', {
+            email,
+            password
+        }, {withCredentials: true});
+    
+        setRedirect(true)
+      
+        if (redirect) {
+            return navigate("/");
+        }
+    }
+
     return (
-        <StyledFormSignIn>
+        <StyledMain>
+            <form onSubmit={submit}>
             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
             <label htmlFor="inputEmail" className="sr-only">Email address</label>
-            <InputEmail type="email" id="inputEmail"  placeholder="Email address" required autoFocus />
+            <InputEmail type="email" id="inputEmail"  
+            onChange={e => setEmail(e.target.value)} placeholder="Email address" required autoFocus />
             <label htmlFor="inputPassword" className="sr-only">Password</label>
-            <InputPassword type="password" id="inputPassword"  placeholder="Password" required />
+            <InputPassword type="password" id="inputPassword"  
+            onChange={e => setPassword(e.target.value)} placeholder="Password" required />
             <div className="checkbox mb-3">
                 <label>
                     <StyledCheckbox type="checkbox" value="remember-me" /> Remember me
                 </label>
             </div>
             <button className="btn btn-sm btn-primary btn-block" type="submit">Sign in</button>
-
-        </StyledFormSignIn>
-
-
+            </form>
+        </StyledMain>
     )
 }
 
